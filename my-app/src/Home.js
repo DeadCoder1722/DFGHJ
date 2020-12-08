6c1f4e6c84fda749a2f5049ea5ab87f6649339d8
ccyccyccy222 <1092098326@qq.com>
2020-12-09 00:01:07 +0800

    完成了页面路由

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,6 +1,28 @@
+import './home.css'
+import React, {Component} from "react";
+
+
 const Home = () => (
-        <p>Hello</p>
+       <div className="abc">
+           <span>Hello</span>
+       </div>
 )
+
+// class Home extends Component {
+//     render() {
+//         return (
+//             <div className="abc">
+//                 <h2>Hello</h2>
+//             </div>
+//         );
+//     }
+// }
+
+// function Home() {
+//     return (
+//
+//     )
+// }
 
 
 export default Home;
